33e1bf0e8de5b74ee2a850d3a17d0ca40b181ef9
Meet Shah <meet1995@gmail.com>
2014-03-22 14:31:28 +0000

    trmplates copied

--- projectx/projectx/urls.py
+++ projectx/projectx/urls.py
@@ -7,7 +7,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'projectx.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',
+     {'next_page': '/'}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/',include('allauth.urls')),
 )

2cc794b09d7a4fee4f0f510f135f7247ce6c0d45
Meet Shah <meet1995@gmail.com>
2014-03-23 06:29:28 +0000

    index url set

--- projectx/projectx/urls.py
+++ projectx/projectx/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'projectx.views.home', name='home'),
+     url(r'^$', 'projectx.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',
